02e56f10a06c5d443795e6f65b6e57c310b27ee9
llamouth <150949923+llamouth@users.noreply.github.com>
2024-06-26 09:46:04 -0400

    feat: complete lab

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,29 +4,23 @@ import '../styles/App.scss'
 function App() {
 
   const [backendData, setBackendData] = useState([])
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8888/pokemon')
-    .then(response => {
-      console.log('Raw response:', response);  // Log the raw response for debugging
-      if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      setBackendData(data);
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-      setError(error.message);
-    });
+    .then( response => response.json() )
+    .then( data => setBackendData(data) )
+    .catch( error => console.error('There has been a problem with your fetch operation:', error) );
   }, []);
 
   return (
     <>
-     
+     <ul>
+      {backendData.map(poke => {
+        return <li>
+          <p>{poke.name}</p>
+        </li>
+      })}
+     </ul>
     </>
   )
 }
